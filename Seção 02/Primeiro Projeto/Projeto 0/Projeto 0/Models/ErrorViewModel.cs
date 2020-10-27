c4de67dff5c1e3a4a09ffa52442741037529383e
FeeZao <felipe.brag99@gmail.com>
2020-10-27 11:38:29 -0300

    primeiro projeto ASP.NET Core MVC

---
+++ Seção 02/Primeiro Projeto/Projeto 0/Projeto 0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
